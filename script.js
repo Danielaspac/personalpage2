7420bbc567bd329f97baaacfe36a9ff6ae8ba570
Daniela Spac <dano4ka3md@mail.ru>
2022-07-26 23:28:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
 var current, array;
 
 
